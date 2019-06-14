8e043f311c737a1b3dc1a9efa7049e0fe343959c
pavanpadi <pavanpadi@HDDLBGKIOSK4.Virtusa.com>
2019-06-14 12:24:27 +0530

    Commit my swap

---
+++ test3/test3/src/main/java/test3/test3/App.java
@@ -0,0 +1,18 @@
+package test3.test3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a=10, b=20,c;
+    	c=a;
+    	a=b;
+    	b=c;
+    	System.out.println("a="+a+"\nb="+b);
+        System.out.println( "Hello World!" );
+    }
+}
